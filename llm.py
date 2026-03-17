072d2923d2cfcf329961a94d88ef43281f7d5471
HAJIRASHAHID <hajirashahid@gmail.com>
2026-03-17 13:28:47 +0500

    new branch

--- llm.py
+++ llm.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 llm = ChatGroq(
     api_key=os.getenv("GROQ_API_KEY"),
-    model="llama-3.3-70b-versatile"
+    model="llama-3.3-70b-versatile" #llama-3.1-8b-instant → bad at tool calling
 )
